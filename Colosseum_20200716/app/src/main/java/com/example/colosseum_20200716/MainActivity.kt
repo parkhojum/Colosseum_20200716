542068f4fc76e09fa5767ba00f3226554a19a69f
박호준 <hankil417@naver.com>
2020-07-16 21:41:50 +0900

    로그인 버튼 누르면 실행되는거

--- Colosseum_20200716/app/src/main/java/com/example/colosseum_20200716/MainActivity.kt
+++ Colosseum_20200716/app/src/main/java/com/example/colosseum_20200716/MainActivity.kt
@@ -2,15 +2,28 @@ package com.example.colosseum_20200716
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.colosseum_20200716.utils.ServerUtil
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
     }
 
     override fun setupEvents() {
+        loginBtn.setOnClickListener {
+
+            //입력한 아이디와 비번을 받아서
+            val inputEmail = emailEdt.text.toString()
+            val inputPw = pwEdt.text.toString()
+
+            //서버에 전달해주고 응답 처리
+            ServerUtil.postRequestLogin(mContext, inputEmail, inputPw, null)
+        }
 
     }
 
