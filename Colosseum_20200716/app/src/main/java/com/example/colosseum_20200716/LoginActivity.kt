3e74849fb4afb70ad2393aa7dbeb992bd811da39
박호준 <hankil417@naver.com>
2020-07-20 21:42:45 +0900

    로그인 성공시 메인화면으로 이동 => 토론 주제 표시 예정

--- Colosseum_20200716/app/src/main/java/com/example/colosseum_20200716/LoginActivity.kt
+++ Colosseum_20200716/app/src/main/java/com/example/colosseum_20200716/LoginActivity.kt
@@ -50,6 +50,12 @@ class LoginActivity : BaseActivity() {
 //                        얻어낸 토큰 저장
                         ContextUtil.setLoginUserToken(mContext, token)
 
+//                        메인화면으로 이동 => 로그인화면은 종류
+                        val myIntent = Intent(mContext,MainActivity::class.java)
+                        startActivity(myIntent)
+
+                        finish()
+
                     }
                     else {
                         //로그인 실패 => 토스트로 실패했다고 출려하자
