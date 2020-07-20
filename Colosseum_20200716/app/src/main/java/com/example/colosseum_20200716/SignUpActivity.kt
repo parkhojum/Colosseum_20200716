89b00f6c3d64126b16d9cd445f76f076921bf7fa
박호준 <hankil417@naver.com>
2020-07-20 19:52:42 +0900

    비번 길이를 입력할때마다 체크

--- Colosseum_20200716/app/src/main/java/com/example/colosseum_20200716/SignUpActivity.kt
+++ Colosseum_20200716/app/src/main/java/com/example/colosseum_20200716/SignUpActivity.kt
@@ -30,7 +30,26 @@ class SignUpActivity : BaseActivity() {
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
 //                내용 변경 완료된 시점에 실행
 
-                Log.d("비번입력", p0.toString())
+//               입력된 글자의 길이 확인.
+//                비어있다면, 비밀번호를 입력해 주세요.
+//                8글자가 안되면, "비밀번호가 너무 짧습니다."
+//                그 이상이면, " 사용해도 좋은 비밀번호 입니다."
+
+                val tempPw = passwordEdt.text.toString()
+
+                if (tempPw.isEmpty()) {
+//                    입력 안한 경우
+                    passworCheckResultTxt.text = "비밀번호를 입력헤 주세요."
+                }
+                else if (tempPw.length < 8){
+//                    길이가 부족한 경우
+                    passworCheckResultTxt.text = "비밀번호가 너무 짧습니다."
+                }
+                else{
+//                    충분히 긴 비밀번호
+                    passworCheckResultTxt.text = "사용해도 좋은 비밀번호 입니다"
+                }
+
             }
 
         })
