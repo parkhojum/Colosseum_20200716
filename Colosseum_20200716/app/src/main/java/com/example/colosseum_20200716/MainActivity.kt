8e4d8a2fcfa15ddf39082b872c627fc0da7b04dc
박호준 <hankil417@naver.com>
2020-07-21 19:49:13 +0900

    Topic 클래스에 json 변환 기능 추가 => 메인 화면에서 JsonArray 파싱때 사용

--- Colosseum_20200716/app/src/main/java/com/example/colosseum_20200716/MainActivity.kt
+++ Colosseum_20200716/app/src/main/java/com/example/colosseum_20200716/MainActivity.kt
@@ -33,7 +33,32 @@ class MainActivity : BaseActivity() {
         ServerUtil.getRequestMainInfo(mContext,object : ServerUtil.JsonResponseHandler{
             override fun onResponse(json: JSONObject) {
 
+                val data = json.getJSONObject("data")
+
+//                topics는 [] 임. => JSONArray 로 추출해야함
+                val topics = data.getJSONArray("topics")
+
+//                topics 내부에는 JSONObject 가 여러개 반복으로 들어 있다.
+//                JSON을 들고 있는 배열 => JSONArray
+
+//                for문을 이용해서, topics 내부의 데이터를 하나씩 추출.
+//                i가0부터 ~topics의 갯수 직전 4개 :(0,1,2,3,)
+
+                for (i in 0 until  topics.length()-1){
+
+//                topics 내부의 데이터를 JSONObect 로 추출
+                    val topicObj = topics.getJSONObject(i)
+
+//                topicObj => Topic 형태의 객체로 변환
+
+                val topic = Topic.getTopicFromJson(topicObj)
+
+//
+//                변환돤 객체를 목록에 추가
+                    mTopicList.add(topic)
             }
+            }
+
         })
 
     }
--- Colosseum_20200716/app/src/main/java/com/example/colosseum_20200716/datas/Topic.kt
+++ Colosseum_20200716/app/src/main/java/com/example/colosseum_20200716/datas/Topic.kt
@@ -1,7 +1,38 @@
 package com.example.colosseum_20200716.datas
 
+import org.json.JSONObject
+
 class Topic {
     var id = 0
     var title = " "
     var imageUrl = " "
+
+    companion object{
+
+        //    json 한 덩어리를 넣으면 => Topic객체로 변환해주는 기능
+
+        fun getTopicFromJson(json:JSONObject) : Topic {
+
+//            변환시켜줄 Topic 객체 생성
+            val topic = Topic()
+
+
+
+//            만든 객체의 내용물을 json을 이용해서 채우자
+            topic.id = json.getInt("id")
+            topic.title = json.getString("title")
+            topic.imageUrl = json.getString("img_url")
+
+
+//            완성된 객체를 리턴
+            return topic
+
+        }
+
+
+
+    }
+
+
+
 }
