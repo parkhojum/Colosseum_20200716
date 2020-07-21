4a5aea23e6bd6b8a06ccbd8cd71705157b592a62
박호준 <hankil417@naver.com>
2020-07-21 20:01:39 +0900

    토론주제 제목만 우선변경

--- Colosseum_20200716/app/src/main/java/com/example/colosseum_20200716/adapters/TopicAdater.kt
+++ Colosseum_20200716/app/src/main/java/com/example/colosseum_20200716/adapters/TopicAdater.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
 import com.example.colosseum_20200716.R
 import com.example.colosseum_20200716.datas.Topic
 import java.math.MathContext
@@ -21,6 +23,13 @@ class TopicAdater(val mContext: Context, val resId : Int, val mList: List<Topic>
         }
 
         val row = tempRow!!
+
+        val topicImg = row.findViewById<ImageView>(R.id.topicImg)
+        val topicTitleTxt = row.findViewById<TextView>(R.id.topicTitleTxt)
+
+        val data = mList[position]
+
+        topicTitleTxt.text = data.title
 
         return row
 
