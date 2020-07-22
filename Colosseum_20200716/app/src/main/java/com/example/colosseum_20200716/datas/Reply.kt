2c4a8e2bc1b9cd70638ee0bc1ceb509d218ecef8
박호준 <hankil417@naver.com>
2020-07-22 21:10:27 +0900

    리스트뷰 아이템 추가

--- Colosseum_20200716/app/src/main/java/com/example/colosseum_20200716/datas/Reply.kt
+++ Colosseum_20200716/app/src/main/java/com/example/colosseum_20200716/datas/Reply.kt
@@ -1,8 +1,37 @@
 package com.example.colosseum_20200716.datas
 
+import org.json.JSONObject
+
 class Reply {
 
+        var id = 0
+    var  content = " "
+    lateinit var writer : User
+    lateinit var selectedSide : Side
 
+    companion object{
+
+//        JSONObje
+
+        fun getReplyFromJson(json : JSONObject) : Reply {
+            val r = Reply()
+
+            r.id = json.getInt("id")
+            r.content = json.getString("content")
+
+
+
+//            작성자 /선택진영 => JSONObject를 받아 곧바로 대입
+            r.writer = User.getUserFromJson()
+            r.selectedSide = Side.getSideFromJson(json.getJSONObject("s"))
+
+            return r
+
+        }
+
+
+
+    }
 
 
 }
---
+++ Colosseum_20200716/app/src/main/java/com/example/colosseum_20200716/datas/User.kt
@@ -0,0 +1,32 @@
+package com.example.colosseum_20200716.datas
+
+import org.json.JSONObject
+
+class User {
+
+    var id = 0
+    var email = " "
+    var nickname = " "
+
+    companion object{
+
+
+
+        fun getUserFromJson(json: JSONObject) : User {
+            val u = User()
+
+            //            사용자 정보를 파싱하는 코드
+            u.id = json.getInt("id")
+            u.email = json.getString("email")
+            u.nickname = json.getString("nickname")
+
+
+
+
+            return u
+
+        }
+
+    }
+
+}
