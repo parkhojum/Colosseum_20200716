2b7666254b44087514d47ef27a7b56681f33982a
박호준 <hankil417@naver.com>
2020-07-29 19:48:54 +0900

    좋아요 / 싫어요 이벤트 코드를 한번에 일괄작성

--- Colosseum_20200716/app/src/main/java/com/example/colosseum_20200716/adapters/ReReplyAdapter.kt
+++ Colosseum_20200716/app/src/main/java/com/example/colosseum_20200716/adapters/ReReplyAdapter.kt
@@ -2,6 +2,8 @@ package com.example.colosseum_20200716.adapters
 
 import android.content.Context
 import android.content.Intent
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,7 +13,9 @@ import com.example.colosseum_20200716.R
 import com.example.colosseum_20200716.ViewReplyDetaActivity
 import com.example.colosseum_20200716.datas.Reply
 import com.example.colosseum_20200716.datas.Topic
+import com.example.colosseum_20200716.utils.ServerUtil
 import com.example.colosseum_20200716.utils.TimeUtil
+import org.json.JSONObject
 import java.text.SimpleDateFormat
 
 class ReReplyAdapter (val mContext: Context, val resId : Int, val mList: List<Reply>)
@@ -48,11 +52,47 @@ class ReReplyAdapter (val mContext: Context, val resId : Int, val mList: List<Re
         likeBtn.text = "좋아요${data.likeCount}"
         dislikeBtn.text = "싫어요${data.dislikeCount}"
 
-        if (data.myLike){
+//        좋아요 / 싫어요 모두 실행하는 코드는 동일함.
+//        서버에 true / false 를 보내는지 , 보내주는 값만 다를 뿐,
+//        두개의 버튼이 눌리면 할 일을(object : ??) => 변수에 담아두고, 버튼에게 붙여만 주자.
+
+        val sendLikeOrDislikeCode = View.OnClickListener {
+
+//            서버에 좋아요 / 싫어요 중 하나를 보내주자.
+//            it 달린 태그값을 => Boolean으로 변환해서 좋아요 / 싫어요를 구별하자
+//            태그 -> String ->  Boolean 의 단계로 변환해야함
+            ServerUtil.postRequesReplyLikeOrDislike(mContext, data.id,it.tag.toString().toBoolean(),object :ServerUtil.JsonResponseHandler{
+                override fun onResponse(json: JSONObject) {
+
+                    val dataObj = json.getJSONObject("data")
+
+                    val reply = Reply.getReplyFromJson(dataObj.getJSONObject("reply"))
+
+                    data.likeCount = reply.likeCount
+                    data.dislikeCount = reply.dislikeCount
+                    data.myLike = reply.myLike
+                    data.myDislike = reply.myDislike
+
+                    val uiHandler = Handler(Looper.getMainLooper())
+
+                    uiHandler.post {
+                        notifyDataSetChanged()
+                    }
+
+                }
+            })
+
+        }
+
+//        좋아요 번튼 /싫어요 버튼이 클릭되면 .sendLikeOrDislikeCode 내부의 내용을 실행하게 하자.
+
+        likeBtn.setOnClickListener(sendLikeOrDislikeCode)
+        dislikeBtn.setOnClickListener(sendLikeOrDislikeCode)
+
+        if (data.myLike) {
             likeBtn.setBackgroundResource(R.drawable.red_border_box)
             likeBtn.setTextColor(mContext.resources.getColor(R.color.naverRed))
-        }
-        else{
+        } else {
             likeBtn.setBackgroundResource(R.drawable.gray_border_box)
             likeBtn.setTextColor(mContext.resources.getColor(R.color.textGray))
         }
@@ -60,11 +100,14 @@ class ReReplyAdapter (val mContext: Context, val resId : Int, val mList: List<Re
         if (data.myDislike) {
             dislikeBtn.setBackgroundResource(R.drawable.blue_border_box)
             dislikeBtn.setTextColor(mContext.resources.getColor(R.color.naverBlue))
-        }
-        else{
+        } else {
             dislikeBtn.setBackgroundResource(R.drawable.gray_border_box)
             dislikeBtn.setTextColor(mContext.resources.getColor(R.color.textGray))
         }
+
+
+
+
 
         return row
     }
