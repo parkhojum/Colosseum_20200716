ab13b6f1b99d5c6d68063f950175852761fc9f5f
박호준 <hankil417@naver.com>
2020-07-29 20:16:20 +0900

    액션바 커스텀마이징 추가

--- Colosseum_20200716/app/src/main/java/com/example/colosseum_20200716/BaseActivity.kt
+++ Colosseum_20200716/app/src/main/java/com/example/colosseum_20200716/BaseActivity.kt
@@ -1,6 +1,8 @@
 package com.example.colosseum_20200716
 
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 abstract class BaseActivity: AppCompatActivity() {
 
@@ -8,6 +10,23 @@ abstract class BaseActivity: AppCompatActivity() {
 
  abstract fun setupEvents()
  abstract fun setValues()
+
+// 액션바에 커스텀으로 바꿔주는 기능
+
+ fun setCustomActionBar() {
+
+//  액션바가 절대 null 아니라고 별개의 변수에 담자.
+  val myActionBar = supportActionBar!!
+
+//  엑션바를 커스텀 엑션바를 쓸 수 있도록 세팅
+  myActionBar.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+  myActionBar.setCustomView(R.layout.custom_action_bar)
+
+//  액션바 뒤의 기본색 제거 => 액션버르 들고 있는 툴바의 좌우 여백(padding)을 0으로 없애자
+  val parentToolBar = myActionBar.customView.parent as Toolbar
+  parentToolBar.setContentInsetsAbsolute(0,0)
+
+ }
 
 
 }
